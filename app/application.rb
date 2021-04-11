73e5426c957705ee7c5382ddfa88c8d9fc007f41
wei chen <weichen9083@gmail.com>
2021-04-11 04:11:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,12 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elseif 
+    elsif req.path.match(/cart/)
+        if @@cart.empty? 
+          resp.wrtie "emptu"
+        else 
+        end 
+      
     else
       resp.write "Path Not Found"
     end
