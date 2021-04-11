b0a000f958c6544d29e913fb80c41f4fbfde82f0
wei chen <weichen9083@gmail.com>
2021-04-11 04:12:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
         if @@cart.empty? 
-          resp.wrtie "Your cart is empty"
+          resp.write "Your cart is empty"
         else 
           @@items.each do |item|
           resp.write "#{item}\n"
