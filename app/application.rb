f514c42b989512c9e1130e55b2ad9d8f30d0b6fc
wei chen <weichen9083@gmail.com>
2021-04-11 04:30:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,12 @@ class Application
         
     elsif req.path.match(/add/)
     add_item = req.params["item"]
-    
+      if @@items.include?(add_item)
+        @@cart << add_item
+        resp.write "added #{add_item}"
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
